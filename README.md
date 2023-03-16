43070c02f95b034f5c718d10a7ca85e7bcbe877a
Ace_Radom <sichenradomlyu@gmail.com>
2023-03-16 08:40:24 +0100

    add /h option

--- README.md
+++ README.md
@@ -42,14 +42,52 @@ Then restart powershell. Your `$profile` should be successfully loaded.
 ## How to use
 
 ```
-sudo [/? | /p]
+sudo [/? | /p [/h]]
 sudo [/b] <commands>
 ```
 
-It will open a new powershell window, access the path where you call sudo, and run all following commands.
+- Using `/?` to show help page.
 
-Using `/?` to show help page.
+- Using `/p` to open a powershell window with admin privileges.
 
-Using `/p` to open a powershell window with admin privileges.
+    - Using `/h` to open the new powershell window in current directory
 
-Using `/b` to run commands in background.
+- Using `/b` to run commands in background.
+
+If you are using sudo directly to run commands, sudo will automatically access the current directory and run all commands you give.
+
+That means, you don't need to set the working directory of the new powershell window with `cd <CurrentDirectory>`, when you need to do so.
+
+**For example**:
+
+I've just used cmake configure a cxx project, now I want to install it with `make install` to the default install path, which is normally `C:\Program Files (x86)`, and I need admin privileges now.
+
+In this situation, you don't need to use sudo like:
+
+```powershell
+sudo "cd $pwd; make install"
+```
+
+but just:
+
+```powershell
+sudo make install
+```
+
+**Last but not least**:
+
+If you want to run more than one commands at the same time with sudo, you must use sudo like that:
+
+```powershell
+sudo "make; make install"
+# 'make' and 'make install' will all be run with admin privileges
+```
+
+Be 100% sure that don't use sudo like that:
+
+```powershell
+sudo make; make install
+# only 'make' will be run with admin privileges
+```
+
+Powershell will divide this line into two commands which are `sudo make` and `make install`, and `make install` will not be run with admin privileges.
--- sudo.ps1
+++ sudo.ps1
@@ -2,28 +2,38 @@ Function _sudo {
     if ( ( $args.Length -eq 0 ) -or ( ( $args.Length -eq 1 ) -and ( $args[0] -eq "/?" ) ) )
     {
         Write-Output "`nSudo Command for Windows PowerShell`n`nTry to run commands with admin privileges`n"
-        Write-Output "Usage: sudo [/? | /p]"
+        Write-Output "Usage: sudo [/? | /p [/h]]"
         Write-Output "Usage: sudo [/b] <commands>`n"
         Write-Output "No Options  show this help page (it is same as typing in /?)"
         Write-Output "/?          show this help page (it is same as not typing in any options)"
-        Write-Output "/p          open one powershell window with admin privileges (it must be used separately)"
+        Write-Output "/p          open one powershell window with admin privileges"
+        Write-Output "/h          open the new powershell window in current directory (it can only be used with /p)"
         Write-Output "/b          run commands in background"
         Write-Output "<commands>  commands need to be run with admin privileges`n"
     } # help page
-    elseif ( ( $args.Length -eq 1 ) -and ( $args[0] -eq "/p" ) ) 
+    elseif ( $args[0] -eq "/p" ) 
     {
-        Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Verb runAs
-#       Start-Process powershell -Verb runAs
+        if ( $args[1] -eq "/h" )
+        {
+            Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Verb runAs -ArgumentList ( "-NoExit" , "cd $pwd" )
+#           Start-Process powershell -Verb runAs -ArgumentList ( "-NoExit" , "cd $pwd" )
+        } # start in current directory
+        else
+        {
+            Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Verb runAs -ArgumentList ( "-NoExit" , "-NoLogo" )
+#           Start-Process powershell -Verb runAs
+        }
     } # start a powershell page with admin privileges
     elseif ( ( $args.Length -gt 1 ) -and ( $args[0] -eq "/b" ) )
     {
         $ss = "cd $pwd ; " + $args[1..$args.Length]
-        Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Verb runAs -WindowStyle Hidden $ss
-    }
+        Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Verb runAs -WindowStyle Hidden -ArgumentList ( $ss )
+#       Start-Process powershell -Verb runAs -WindowStyle Hidden $ss
+    } # run in background
     else
     {
-        $ss = "cd $pwd ; $args ; pause"
-        Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Verb runAs $ss
+        $ss = "$args ; pause"
+        Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Verb runAs -ArgumentList ( $ss )
 #       Start-Process powershell -Verb runAs $ss
     } # run command (default)
 
